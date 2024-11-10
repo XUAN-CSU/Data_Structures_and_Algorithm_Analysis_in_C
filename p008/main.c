947530c6de2985df8b367869f58db4ff9e3751ef
WLC <Armstrong_Number@outlook.com>
2024-11-09 22:22:55 -0900

    recursion illustration

---
+++ p008/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int F(int x) 
+{
+	if (x == 0) {
+		return 0;
+	} else {
+		return 2 * F(x - 1) + x * x;
+	}
+}
+
+int main()
+{
+	int a  = 4;
+	printf("F(4) is : %d\n", F(4));
+	return 0;
+}
+
